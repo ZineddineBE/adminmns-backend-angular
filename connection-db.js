010cf6b7ce23347a88528b028525c0f004de217a
ZineddineBE <beouche.zineddine@gmail.com>
2025-06-26 14:15:10 +0200

    Ajout de variables d'environnement pour la connexion à la db

--- connection-db.js
+++ connection-db.js
@@ -1,20 +1,20 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
-// Configuration de la base de données
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Adminmns2025+",
-  database: "project-angular",
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
 });
 
-// Connexion à la base de données
 connection.connect((err) => {
-  if (err) {
-    console.error("Erreur de connexion à la base de données :", err);
-    return;
-  }
-  console.log("Connecté à la base de données MySQL");
+	if (err) {
+		console.error("Erreur de connexion à la base de données :", err);
+		return;
+	}
+	console.log("Connecté à la base de données MySQL");
 });
 
-module.exports = connection
+module.exports = connection;
